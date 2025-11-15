9332a2f85b3373e8fff6939efaa21bea7a510d15
monteiroraquel <raquel6monteiro@gmail.com>
2025-11-14 21:56:13 -0300

    criação da classe produto

---
+++ Barbearia/src/main/java/produto/Produto.java
@@ -0,0 +1,61 @@
+
+package produto;
+
+public class Produto {
+
+    private int id;
+    private String nome;
+    private double preco;
+    private int quantidadeEstoque;
+    private String categoria;
+
+    public Produto(int id, String nome, double preco, int quantidadeEstoque, String categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEstoque = quantidadeEstoque;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    
+}
