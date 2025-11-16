de5e9e0c9a01432283f375169b2e2a3f29020f41
eduardafernandessz <eduarda.fernandes@ufvjm.edu.br>
2025-11-15 23:59:28 -0300

    criação classe teste para perguntas

---
+++ Barbearia/src/main/java/teste/testes.java
@@ -0,0 +1,13 @@
+package teste;
+
+public class testes {
+
+    public static void main(String[] args) {
+
+        System.out.println("Iniciando Barbearia...");
+
+        teste.questao q = new teste.questao(); // ou import teste.questoes
+        q.questao15();
+
+    }
+}
